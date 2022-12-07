92d61334dfda8e24c6db1cd3a1da8cefb6a7b02c
JongHoonC <bjhlove0126@naver.com>
2022-12-07 14:01:04 +0900

    fullpage

--- express/public/js/src.js
+++ express/public/js/src.js
@@ -1,10 +1,38 @@
+// new fullpage('#fullpage', {
+//   //하단은 옵션
+//   autoScrolling: true,
+//   scrollHorizontally: true,
+//   navigation: true,
+//   fixedElements: '#h_wrap, #scroll-downs',
+//   responsiveWidth: 768,
+// });
+
+AOS.init(); // AOS initiation
+AOS.init({
+  disable: function () {
+    var maxWidth = 768;
+    return window.innerWidth < maxWidth;
+  },
+});
+$('.aos-init').removeClass('aos-animate'); // remove ALL classes which triggers animation in document
+
 new fullpage('#fullpage', {
-  //하단은 옵션
+  anchors: ['1', '2', '3', '4', '5', '6', '7', '8'],
+
+  afterLoad: function () {
+    var a_table = ['1', '2', '3', '4', '5', '6', '7', '8']; // duplicated table of anchors name
+
+    for (var i = 0; i < a_table.length; i++) {
+      $('.section-' + a_table[i] + '.active .aos-init').addClass('aos-animate'); // all magic goes here - when page is active, then all elements with AOS will start animate
+    }
+  },
   autoScrolling: true,
   scrollHorizontally: true,
   navigation: true,
   fixedElements: '#h_wrap, #scroll-downs',
+  scrollingSpeed: 900,
   responsiveWidth: 768,
+  // responsive: true,
 });
 
 var swiper = new Swiper('.mySwiper', {
