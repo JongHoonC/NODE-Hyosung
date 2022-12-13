c16b781591ed538b34a06f21b8c01c3f915480bb
JongHoonC <bjhlove0126@naver.com>
2022-12-13 17:33:19 +0900

    history

--- express/public/js/employment.js
+++ express/public/js/employment.js
@@ -70,17 +70,6 @@ function scrollBtn() {
   }
 }
 
-// window.onscroll = function () {
-//   fixedNav();
-// };
-// function fixedNav() {
-// if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-//   fixedNav.classList.add('active');
-// } else {
-//   fixedNav.classList.remove('active');
-//   }
-// }
-
 window.addEventListener('scroll', event => {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     fixedNav.classList.add('active');
@@ -88,3 +77,44 @@ window.addEventListener('scroll', event => {
     fixedNav.classList.remove('active');
   }
 });
+
+let year = document.querySelectorAll('.nav_year');
+for (let i = 0; i < year.length; i++) {
+  year[i].addEventListener('click', () => {
+    year[i].classList.toggle('active');
+  });
+  year[i].addEventListener('click', () => {
+    year[i].classList.remove('active');
+  });
+}
+
+function checkVisible(element, check = 'above') {
+  const viewportHeight = $(window).height(); // Viewport Height
+  const scrolltop = $(window).scrollTop(); // Scroll Top
+  const y = $(element).offset().top;
+  const elementHeight = $(element).height();
+  console.log(scrolltop);
+
+  // 반드시 요소가 화면에 보여야 할경우
+  if (check == 'visible') return y < viewportHeight + scrolltop && y > scrolltop - elementHeight;
+
+  // 화면에 안보여도 요소가 위에만 있으면 (페이지를 로드할때 스크롤이 밑으로 내려가 요소를 지나쳐 버릴경우)
+  if (check == 'above') return y < viewportHeight + scrolltop;
+}
+
+// 리소스가 로드 되면 함수 실행을 멈출지 말지 정하는 변수
+
+let isVisible = false;
+// 이벤트에 등록할 함수
+const func = function () {
+  let isVisible = false;
+  if (!isVisible && checkVisible('#1988')) {
+    isVisible = true;
+  }
+
+  // 만일 리소스가 로드가 되면 더이상 이벤트 스크립트가 있을 필요가 없으니 삭제
+  // isVisible && window.removeEventListener('scroll', func);
+};
+
+// 스크롤 이벤트 등록
+window.addEventListener('scroll', func);
