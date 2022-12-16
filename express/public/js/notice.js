3a3dd31358a286c402c50326ab4dbcfce484945d
JongHoonC <bjhlove0126@naver.com>
2022-12-16 11:11:48 +0900

    css컨벤션

--- express/public/js/notice.js
+++ express/public/js/notice.js
@@ -67,3 +67,19 @@ function scrollBtn() {
     h_wrap.classList.remove('focus');
   }
 }
+
+function filter() {
+  var value, name, item, i;
+
+  value = document.getElementById('value').value.toUpperCase();
+  item = document.getElementsByClassName('list');
+
+  for (i = 0; i < item.length; i++) {
+    name = item[i].getElementsByClassName('title');
+    if (name[0].innerHTML.toUpperCase().indexOf(value) > -1) {
+      item[i].style.display = 'flex';
+    } else {
+      item[i].style.display = 'none';
+    }
+  }
+}
