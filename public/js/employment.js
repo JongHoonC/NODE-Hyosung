c59bcc854c9aa0e4535682bba03864a225e3d116
Jhoon <bjhlove0126@naver.com>
2022-12-19 22:26:30 +0900

    scss  변환 완료  히스토리 적용하기

--- public/js/employment.js
+++ public/js/employment.js
@@ -78,36 +78,6 @@ window.addEventListener('scroll', event => {
   }
 });
 
-function checkVisible(element, check = 'above') {
-  const viewportHeight = $(window).height(); // Viewport Height
-  const scrolltop = $(window).scrollTop(); // Scroll Top
-  const y = $(element).offset().top;
-  const elementHeight = $(element).height();
-  // 반드시 요소가 화면에 보여야 할경우
-  // if (check == 'visible') return y < viewportHeight + scrolltop && y > scrolltop - elementHeight;
-
-  // 화면에 안보여도 요소가 위에만 있으면
-  // (페이지를 로드할때 스크롤이 밑으로 내려가 요소를 지나쳐 버릴경우)
-  if (check == 'above') return y < scrolltop + 200;
-}
-let isVisible = false;
-// 이벤트에 등록할 함수
-const func = function () {
-  let isVisible = false;
-  let navyear = document.querySelectorAll('.nav_year');
-  let year = document.querySelectorAll('.year');
-  for (j = 0; j < year.length; j++) {
-    if (!isVisible && checkVisible('#1999')) {
-      console.log(year[j]);
-      year[j].classList.add('active');
-    }
-    isVisible = true;
-  }
-};
-
-// 스크롤 이벤트 등록
-window.addEventListener('scroll', func);
-
 $(() => {
   let stickyTop = 0,
     scrollTarget = false;
